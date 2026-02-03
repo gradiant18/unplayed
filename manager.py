0cade5e2adf55423a704e74ff71c5179bd1051c5
gradiant <gradiant@duck.com>
2026-02-03 11:55:18 -0400

    added feature to get medal type for completed maps

--- manager.py
+++ manager.py
@@ -11,6 +11,7 @@ current_dir = "/home/russell/.local/share/Steam/steamapps/compatdata/7200/pfx/dr
 unplayed_dir = "/home/russell/.local/share/Steam/steamapps/compatdata/7200/pfx/drive_c/users/steamuser/Documents/TrackMania/Tracks/Challenges/Unplayed"
 autosaves_dir = "/home/russell/.local/share/Steam/steamapps/compatdata/7200/pfx/drive_c/users/steamuser/Documents/TrackMania/Tracks/Replays/Autosaves"
 
+
 def clean_autosave(path):
     directory, filename = os.path.split(path)
     clean_name = re.sub(r"[^a-zA-Z0-9_\\[\]\$\(\)\.\ -]", "", filename)
@@ -19,6 +20,38 @@ def clean_autosave(path):
     if path != clean_path:
         os.rename(path, clean_path)
     return clean_path
+
+
+def get_medal(autosave, track_id):
+    ghost = Gbx(autosave).get_class_by_id(GbxType.CTN_GHOST)
+    if not ghost:
+        return None
+
+    url = "https://tmnf.exchange/api/tracks"
+    params = {
+        "fields": "AuthorTime,GoldTarget,SilverTarget,BronzeTarget",
+        "id": track_id,
+    }
+
+    try:
+        response = requests.get(url, params=params, timeout=10)
+        response.raise_for_status()
+        medals = response.json().get("Results")[0]
+    except requests.RequestException as e:
+        print(f"Request Error: {e}")
+        return None
+
+    if ghost.race_time <= medals["AuthorTime"]:
+        return "author"
+    elif ghost.race_time <= medals["GoldTarget"]:
+        return "gold"
+    elif ghost.race_time <= medals["SilverTarget"]:
+        return "silver"
+    elif ghost.race_time <= medals["BronzeTarget"]:
+        return "bronze"
+    else:
+        return "none"
+
 
 def get_track_name(path):
     g = Gbx(path)
@@ -31,11 +64,13 @@ def get_track_name(path):
     g.f.close()
     return clean_name
 
+
 def get_track_id(path):
     match = re.search(r"\/\d+\.", path)
     if not match:
         return None
     return match.group()[1:-1]
+
 
 def has_record(track_id):
     url = "https://tmnf.exchange/api/tracks"
@@ -49,6 +84,7 @@ def has_record(track_id):
         print(f"Request Error: {e}")
         return False
 
+
 def scan_tracks(path):
     tracks = []
     for entry in os.scandir(path):
@@ -57,9 +93,10 @@ def scan_tracks(path):
 
         track_id = get_track_id(entry.path)
         track_name = get_track_name(entry.path)
-        tracks.append({"path":entry,"id":track_id,"name":track_name})
+        tracks.append({"path": entry, "id": track_id, "name": track_name})
     return tracks
-            
+
+
 def scan_replays(path):
     replays = []
     for entry in os.scandir(path):
@@ -67,12 +104,14 @@ def scan_replays(path):
             continue
         entry = clean_autosave(entry.path)
         _, filename = os.path.split(entry)
-        replays.append(filename)
+        replays.append({"path": entry, "name": filename})
     return replays
 
+
+medals_collected = {"author": 0, "gold": 0, "silver": 0, "bronze": 0, "none": 0}
 unplayed = scan_tracks(unplayed_dir)
 current = scan_tracks(current_dir)
-for track in current: # remove played maps from current
+for track in current:  # remove played maps from current
     if has_record(track["path"]):
         os.remove(track["path"])
         current.pop(track)
@@ -82,14 +121,21 @@ while True:
     autosaves = scan_replays(autosaves_dir)
 
     for track in current:
-        if f'steamuser_{track["name"]}.Replay.gbx' in autosaves:
-            print(f'{track["name"]} is finished')
-            # move track into finished directory
-            _, file = os.path.split(track["path"])
-            new_path = os.path.join(finished_dir, file)
-            os.rename(track["path"], new_path)
+        for autosave in autosaves:
+            if autosave["name"] == f"steamuser_{track["name"]}.Replay.gbx":
+                print(f'{track["name"]} is finished')
 
-            current.remove(track) # update current
+                # get medal type
+                medal = get_medal(autosave["path"], track["id"])
+                medals_collected[medal] += 1
+                print(medals_collected)
+
+                # move track into finished directory
+                _, file = os.path.split(track["path"])
+                new_path = os.path.join(finished_dir, file)
+                os.rename(track["path"], new_path)
+
+                current.remove(track)  # update current
 
     while len(current) < 2:
         track = unplayed.pop()
